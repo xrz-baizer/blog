dfb297dce2d3affc356bfaf3cdb9b1d7168518c8
XRZ <397047243@qq.com>
2024-11-19 11:41:36 +0800

    auto commit

--- deploy.sh
+++ deploy.sh
@@ -58,7 +58,7 @@ echo "静态文件构建成功。"
 
 # 上传静态文件到云服务器
 echo "==========> 上传静态文件到云服务器..."
-rsync -av --delete "$BLOG_PATH/docs/.vitepress/dist/" "$REMOTE_SERVER:$REMOTE_PATH/"
+rsync -av --delete --iconv=UTF-8,UTF-8 "$BLOG_PATH/docs/.vitepress/dist/" "$REMOTE_SERVER:$REMOTE_PATH/"
 if [ $? -ne 0 ]; then
   echo "静态文件上传失败，请检查！"
   exit 1
