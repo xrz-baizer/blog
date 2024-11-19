8ee106f28a0b3dd13675aade2e79ed7ba6d74282
XRZ <397047243@qq.com>
2024-11-19 11:38:12 +0800

    auto commit

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,12 @@
 # 定义变量
 REPO_PATH="/Users/xrz/Library/Mobile Documents/com~apple~CloudDocs/KnowledgeRepository"
 BLOG_PATH="/Users/Work/Pagoda/this/Blog"
-# 同步的文件夹
 SYNC_DIRS=("00-TechnicalFile" "01-Essay" "02-English" "Image")
 
-# 远程服务器信息
 REMOTE_SERVER="root@cloudserver"
 REMOTE_PATH="/app"
 
-# 默认Commit
+# 默认 Commit 信息
 if [ -z "$1" ]; then
   COMMIT="auto commit"
 else
@@ -18,8 +16,8 @@ else
 fi
 
 # 自动提交并推送代码
-echo "==========>开始提交代码..."
-cd "$BLOG_PATH"
+echo "==========> 开始提交代码..."
+cd "$BLOG_PATH" || { echo "无法进入 $BLOG_PATH，请检查路径！"; exit 1; }
 git add .
 git commit -m "$COMMIT"
 git push
@@ -29,32 +27,14 @@ if [ $? -ne 0 ]; then
 fi
 echo "代码提交并推送成功。"
 
-# 同步文件到目标目录
-echo "==========>移动文档到 Blog 项目中..."
+# 同步文件到 Blog 项目目录
+echo "==========> 同步文档到 Blog 项目中..."
 for DIR in "${SYNC_DIRS[@]}"; do
   SOURCE_DIR="$REPO_PATH/$DIR"
   TARGET_DIR="$BLOG_PATH/docs/$DIR"
 
   if [ -d "$SOURCE_DIR" ]; then
-    # 删除目标目录
-    if [ -d "$TARGET_DIR" ]; then
-      rm -rf "$TARGET_DIR"
-      if [ $? -ne 0 ]; then
-        echo "删除目标目录 $TARGET_DIR 失败，请检查！"
-        exit 1
-      fi
-      echo "目标目录 $TARGET_DIR 已删除。"
-    fi
-
-    # 创建目标目录
-    mkdir -p "$TARGET_DIR"
-    if [ $? -ne 0 ]; then
-      echo "创建目标目录 $TARGET_DIR 失败，请检查！"
-      exit 1
-    fi
-
-    # 同步文件
-    scp -r "$SOURCE_DIR/" "$TARGET_DIR/"
+    rsync -av --delete "$SOURCE_DIR/" "$TARGET_DIR/"
     if [ $? -ne 0 ]; then
       echo "$DIR 文件同步失败，请检查！"
       exit 1
@@ -64,10 +44,11 @@ for DIR in "${SYNC_DIRS[@]}"; do
     echo "源目录 $SOURCE_DIR 不存在，跳过同步。"
   fi
 done
-echo "文档移动成功。"
+echo "文档同步完成。"
 
 # 构建静态文件
-echo "==========>开始构建静态文件..."
+echo "==========> 开始构建静态文件..."
+cd "$BLOG_PATH" || { echo "无法进入 $BLOG_PATH，请检查路径！"; exit 1; }
 yarn build
 if [ $? -ne 0 ]; then
   echo "静态文件构建失败，请检查！"
@@ -76,9 +57,8 @@ fi
 echo "静态文件构建成功。"
 
 # 上传静态文件到云服务器
-echo "==========>上传静态文件到云服务器..."
-ssh "$REMOTE_SERVER" "mkdir -p $REMOTE_PATH"
-scp -r "$BLOG_PATH/docs/.vitepress/dist/"* "$REMOTE_SERVER:$REMOTE_PATH/"
+echo "==========> 上传静态文件到云服务器..."
+rsync -av --delete "$BLOG_PATH/docs/.vitepress/dist/" "$REMOTE_SERVER:$REMOTE_PATH/"
 if [ $? -ne 0 ]; then
   echo "静态文件上传失败，请检查！"
   exit 1
